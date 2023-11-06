415da648513345649c777f58a64c6b1f447239ba
ansif-ar88 <125662944+ansif-ar88@users.noreply.github.com>
2023-11-06 16:22:59 +0530

    index update 4

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ const server = app.listen(process.env.PORTNUMBER,()=>{
 const io = new Server(server, {
     pingTimeout: 60000,
     cors: {
-      origin: ['http://localhost:5173',"https://virtualcare.vercel.app"]
+      origin: ['http://localhost:5173',"https://virtualcares.vercel.app"]
     }
   });
   io.on("connection",(socket)=>{
